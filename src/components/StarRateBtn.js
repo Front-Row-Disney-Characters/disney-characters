cc5f4482bda60fc7b52f4494ce0ce56e76382dac
BigThinkerYes <designaspnet4@hotmail.com>
2022-12-05 16:07:49 -0600

    setting up add btn

---
+++ src/components/StarRateBtn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {StarSolid} from "./svg/star-solid.svg";
+// import {StarRegular} from "./svg/star-regular.svg";
+// import {StarHalf} from "./svg/star-half-stroke-solid.svg";
+import Button from "react-bootstrap/Button";
+
+const StarRateBtn =()=>{
+   
+
+    return(
+        <div>
+           <Button variant='primary'>Add Me.</Button>
+        </div>
+    )
+}
+export {StarRateBtn}
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,11 +4,14 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import Col from "react-bootstrap/Col";
 import "./css/btnStyle.css";
+import { StarRateBtn } from './StarRateBtn';
 
 
 function UserCard({ id, url, name, userFilteredList }) {
 
   const [user, setUser] = useState(null);
+  // added
+  const[userChoice, setUserChoice]= useState('');
 
   const fetchUser = async () => {
     // render the first page of 50 characters (array of id, name, image, url)
@@ -28,9 +31,20 @@ function UserCard({ id, url, name, userFilteredList }) {
             <div className="card mb-3 w-100 h-100 indiv-card d-flex btnStyle" >
                 <div className="row g-0">
                     <div className="col-md-6 my-auto">
-                      <img src={`${user.imageUrl}`} className="img-fluid rounded-start pt-3 ps-2"  class="image" alt="disney characters"/>
+                      <img src={`${user.imageUrl}`} className="img-fluid rounded-start pt-3 ps-2"   class="image" alt="disney characters" 
+                      value={userChoice.imageUrl} 
+                      onChange={event=>{
+                        setUserChoice(event.target.value) 
+                      }}/>
                       <div className="overlay overlayLeft">
-                        <div className="text">hello</div>
+                        <div className="text">
+                          <Link to={`/characters/${id}`} style={{color: "black"}}>
+                            <h5 className="card-title cardImgTitle">{user.name}</h5>
+                          </Link>
+                        </div>
+                        <div className='starContainer'>
+                          <StarRateBtn />
+                        </div>
                       </div>
                     </div>
                     <div className="col-md-6 my-auto">
