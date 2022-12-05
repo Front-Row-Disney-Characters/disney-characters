6f04571a7f7a4f5eefa7f33e98aed6266f3f0d65
BigThinkerYes <designaspnet4@hotmail.com>
2022-12-05 14:22:11 -0600

    btn started process Lavor

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # disney characters
+## first step: 
+# npm install
+## second step:
+# npm run start
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import Col from "react-bootstrap/Col";
+import "./css/btnStyle.css";
 
 
 function UserCard({ id, url, name, userFilteredList }) {
@@ -24,10 +25,13 @@ function UserCard({ id, url, name, userFilteredList }) {
   return (
     <>
       {user ? (
-            <div className="card mb-3 w-100 h-100 indiv-card d-flex" >
+            <div className="card mb-3 w-100 h-100 indiv-card d-flex btnStyle" >
                 <div className="row g-0">
                     <div className="col-md-6 my-auto">
-                      <img src={`${user.imageUrl}`} className="img-fluid rounded-start pt-3 ps-2" alt="disney characters"/>
+                      <img src={`${user.imageUrl}`} className="img-fluid rounded-start pt-3 ps-2"  class="image" alt="disney characters"/>
+                      <div className="overlay overlayLeft">
+                        <div className="text">hello</div>
+                      </div>
                     </div>
                     <div className="col-md-6 my-auto">
                       <div className="card-body">
